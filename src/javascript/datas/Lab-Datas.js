7b18fc8a7116e68f19778d9bed0b059838072f38
MelinaGuyon <guyonmelina@gmail.com>
2017-04-19 19:57:14 +0200

    Add links in lab

--- src/javascript/datas/Lab-Datas.js
+++ src/javascript/datas/Lab-Datas.js
@@ -4,25 +4,29 @@ const labDatas = {
       title: 'Shader 2D',
       date: 'Janvier 2017',
       techno: 'GLSL',
-      src : 'assets/labs/shader2D.mov'
+      src : 'assets/labs/shader2D.mov',
+      codepenUrl: 'http://codepen.io/MelinaGuyon/pen/QpmwwO'
     },
     {
       title: 'Shape Generation',
       date: 'Decembre 2016',
       techno: 'Three.js',
-      src : 'assets/labs/shape.mov'
+      src : 'assets/labs/shape.mov',
+      codepenUrl : 'http://codepen.io/MelinaGuyon/pen/NdLMWy'
     },
     {
       title: 'Audio Fire',
       date: 'Novembre 2016',
       techno: 'Web audio API',
-      src : 'assets/labs/fire.mov'
+      src : 'assets/labs/fire.mov',
+      codepenUrl: 'http://codepen.io/MelinaGuyon/pen/LRKpRb'
     },
     {
       title: 'Mouse Following',
       date: 'Octobre 2016',
       techno: 'Canvas',
-      src : 'assets/labs/triangles.mov'
+      src : 'assets/labs/triangles.mov',
+      codepenUrl: 'http://codepen.io/MelinaGuyon/pen/vXrOwd'
     }
   ]
 }
--- src/modules/Lab.vue
+++ src/modules/Lab.vue
@@ -2,11 +2,10 @@
   <div id="lab">
     <div v-for="lab in datas.labs" class="lab js-slider-lab">
       <div class="lab__video__container">
-        <video :src="lab.src" poster="" loop>
-          Votre navigateur ne permet pas de lire les vidéos.
-          Mais vous pouvez toujours aller voir l'xp sur
-          <a href="http://codepen.io/MelinaGuyon/pen/QpmwwO">code pen</a> !
-        </video>
+        <a :href="lab.codepenUrl" target="_blank">
+          <video :src="lab.src" poster="" loop>
+          </video>
+        </a>
       </div>
       <div class="lab__desc">
         <div class="desc">
